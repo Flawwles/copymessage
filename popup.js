2d3f4ba7e3049b36ccb584e42e1be7fbadf60791
Matthew Evans <matt_-@hotmail.co.uk>
2016-01-12 13:34:16 +0000

    Changes to loading JSON

--- popup.js
+++ popup.js
@@ -1,33 +1,41 @@
-
-  // Once the DOM is ready...
+// Once the DOM is ready...
 window.addEventListener('DOMContentLoaded', function() {
-// Update the relevant fields with the new data
+  // Update the relevant fields with the new data
   function setDOMInfo(info) {
-    document.getElementById('page-title').value = JSON.parse(info.divHtml);
+    // document.getElementById('page-title').value = JSON.parse(info.divHtml);
     saveChanges(info);
   }
-
   chrome.tabs.query({
     active: true,
     currentWindow: true
   }, function(tabs) {
     chrome.tabs.sendMessage(tabs[0].id, {
-        from: 'popup',
-        subject: 'DOMInfo'
-      },setDOMInfo);
+      from: 'popup',
+      subject: 'DOMInfo'
+    }, setDOMInfo);
   });
+  // Set values / Event listeners
+  document.getElementById('btn-copy').addEventListener('click', updateObj);
+  // document.getElementById('text-area').addEventListener('keyup', updateObj);
+  document.getElementById('btn-clear').addEventListener('click', clearStore);
+  document.getElementById('date').value = uniq;
+  // Global vars
+ var theValue = document.getElementById('text-area').value,
+     theDate = document.getElementById('date').value,
+     uniq = (new Date()).getTime(),
+     messagesList = [];
 
-  document.getElementById('text-area').addEventListener('keyup', saveChanges);
+function updateObj(){
+    theValue = document.getElementById('text-area').value;
+    messagesList.push({'value' : theValue, 'date' : uniq});
+    saveChanges();
+}
 
-  function saveChanges(emailInfo) {
-    var theValue = document.getElementById('text-area').value;
 
-    // if (!theValue) {
-    //   console.log('Error: No value specified');
-    //   return;
-    // }
+function saveChanges(emailInfo) {
+
     chrome.storage.local.set({
-      'value': theValue
+      'messagesList': JSON.stringify(messagesList)
     }, function() {
       // Notify that we saved.
       console.log('Settings saved');
@@ -37,12 +45,16 @@ window.addEventListener('DOMContentLoaded', function() {
   function loadChanges() {
     var theValue = [];
     chrome.storage.local.get({
-      'value': theValue
+      'messagesList': JSON.stringify(messagesList)
     }, function(results) {
       // console.log('Value = ' + results.value);
-      document.getElementById('text-area').value = results.value;
+      document.getElementById('text-area-output').value = results.messagesList;
     });
   }
   loadChanges();
-});
 
+  function clearStore() {
+    chrome.storage.local.clear();
+  }
+
+});
